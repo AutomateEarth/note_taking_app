24821ba12fc019b93540855655126b3ebc4bf780
Jim <jpstrategy_clr@outlook.com>
2020-04-22 16:49:11 -0400

    process, filesystem, validator, yargs, chalk

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
-const fs = require('fs')
+const chalk = require('chalk')
+const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-// fs.writeFileSync('notes.txt', 'My name is Jim.')
+// Customize yargs version
+yargs.version('1.1.0')
 
-fs.appendFileSync('notes.txt', 'My last name is Peterson')
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note...')
+    }
+})
+
+// Create a list command
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function () {
+        console.log('Listing out all notes...')
+    }
+})
+
+// Create a read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note...')
+    }
+})
+
+
+yargs.parse()
